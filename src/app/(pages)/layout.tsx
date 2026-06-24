5588a7e40020a6d67ba182282290b7b5331ec492
Core-SebastianParejo <sparejo@coreeb.com>
2026-06-24 12:27:45 -0500

    feat(layout): implement layout component with header and sidebar

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,9 +1,17 @@
-import { type ReactNode } from 'react'
+import { type ReactNode } from "react";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
-const PagesLayout = ({children}: {children: ReactNode}) => {
-    return( 
-    <div>{children}</div>
-)
-}
+const PagesLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        {children}
+      </div>
+    </div>
+  );
+};
 
-export default PagesLayout
+export default PagesLayout;
