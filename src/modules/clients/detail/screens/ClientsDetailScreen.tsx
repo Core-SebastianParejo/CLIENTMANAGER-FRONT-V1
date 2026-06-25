9e5c18ceb52f2e13e63f4aa4f124284bfed1a294
Core-SebastianParejo <sparejo@coreeb.com>
2026-06-25 15:26:01 -0500

    feat(clients): implement client detail screen with data fetching and display

--- src/modules/clients/detail/screens/ClientsDetailScreen.tsx
+++ src/modules/clients/detail/screens/ClientsDetailScreen.tsx
@@ -1,7 +1,38 @@
 "use client";
+import { useEffect, useState } from "react";
+import { useDetailClient } from "../hooks/useDetailClient";
+import { useRouter } from "next/navigation";
+import { ROUTES } from "@/lib/constants/routes";
 
-const ClientDetailScreen = () => {
-  return <div>Register</div>;
+const ClientDetailScreen = ({ id }: { id: string }) => {
+  const { client, loading, getClient } = useDetailClient();
+  const router = useRouter();
+
+  useEffect(() => {
+    getClient(id);
+  }, [id, getClient]);
+
+  if (loading) return <div>Cargando...</div>;
+
+  return (
+    <div>
+      <div>
+        <p>{client?.fullName}</p>
+        <p>{client?.email}</p>
+        <p>{client?.phone}</p>
+        <p>{client?.company}</p>
+        <p>{client?.createdAt}</p>
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={() => client && router.push(ROUTES.clientEdit(client.id))}
+        >
+          Ir a Editar
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ClientDetailScreen;
