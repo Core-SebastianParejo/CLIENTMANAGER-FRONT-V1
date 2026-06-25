a4a1c0ef975b127669fc5a2ab73fce50d4c581ce
Core-SebastianParejo <sparejo@coreeb.com>
2026-06-25 10:27:19 -0500

    feat(clients): update client routes and implement client list screen with data fetching

--- src/lib/constants/routes.ts
+++ src/lib/constants/routes.ts
@@ -1,8 +1,8 @@
 export const ROUTES = {
   clients: "/clients",
-  clientsCreate: "/clients/create",
-  clientsDetails: (id: string) => `/clients/${id}`,
-  clientsEdit: (id: string) => `/clients/${id}/edit`,
+  clientCreate: "/clients/create",
+  clientDetails: (id: string) => `/clients/${id}`,
+  clientEdit: (id: string) => `/clients/${id}/edit`,
   login: "/login",
   register: "/register",
   dashboard: "/dashboard",
--- src/modules/clients/list/screens/ClientsListScreen.tsx
+++ src/modules/clients/list/screens/ClientsListScreen.tsx
@@ -1,7 +1,46 @@
 "use client";
+import { useListClient } from "../hooks/useClient";
+import { useRouter } from "next/navigation";
+import { ROUTES } from "@/lib/constants/routes";
 
 const ClientsListScreen = () => {
-  return <div>Register</div>;
+  const { clients, loading } = useListClient();
+  const router = useRouter();
+  if (loading) return <div>Cargando...</div>;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Company</th>
+          <th>Created Date</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {clients.map((client) => (
+          <tr key={client.id}>
+            <td>{client.fullName}</td>
+            <td>{client.email}</td>
+            <td>{client.phone}</td>
+            <td>{client.company}</td>
+            <td>{client.createdAt}</td>
+            <td>
+              <button onClick={() => router.push(ROUTES.clientEdit(client.id))}>
+                Editar
+              </button>
+            </td>
+            <td>
+              <button onClick={() => console.log(client.id)}>Eliminar</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default ClientsListScreen;
