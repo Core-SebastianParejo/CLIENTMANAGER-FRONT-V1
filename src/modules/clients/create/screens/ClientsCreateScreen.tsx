5d88788755ee63ec4b36cd99b58fd13727ddaf06
Core-SebastianParejo <sparejo@coreeb.com>
2026-06-25 14:58:19 -0500

    feat(clients): implement client creation form with input handling and submission

--- src/modules/clients/create/screens/ClientsCreateScreen.tsx
+++ src/modules/clients/create/screens/ClientsCreateScreen.tsx
@@ -1,7 +1,58 @@
 "use client";
+import { useState } from "react";
+import { useCreateClient } from "../hooks/useCreateClient";
+import { useRouter } from "next/navigation";
+import { ROUTES } from "@/lib/constants/routes";
 
 const ClientsCreateScreen = () => {
-  return <div>Register</div>;
+  const [fullNameInput, setFullNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [phoneInput, setPhoneInput] = useState("");
+  const [companyInput, setCompanyInput] = useState("");
+  const { createClient, loading } = useCreateClient();
+  const router = useRouter();
+
+  return (
+    <form>
+      <div>
+        <input
+          value={fullNameInput}
+          type="text"
+          onChange={(e) => setFullNameInput(e.target.value)}
+        />
+        <input
+          value={emailInput}
+          type="email"
+          onChange={(e) => setEmailInput(e.target.value)}
+        />
+        <input
+          value={phoneInput}
+          type="text"
+          onChange={(e) => setPhoneInput(e.target.value)}
+        />
+        <input
+          value={companyInput}
+          type="text"
+          onChange={(e) => setCompanyInput(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <button
+          disabled={loading}
+          type="submit"
+          onClick={() =>
+            createClient(fullNameInput, emailInput, phoneInput, companyInput)
+          }
+        >
+          Crear
+        </button>
+        <button type="button" onClick={() => router.push(ROUTES.clients)}>
+          Cancelar
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default ClientsCreateScreen;
